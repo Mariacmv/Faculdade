46049f26f1e0a9c5b80d809ef9f6719d47bef227
Mariacmv <you@example.com>
2025-03-11 10:24:06 -0300

    Anotações aula2

---
+++ array_funcao_objeto.js
@@ -0,0 +1,44 @@
+//Declaração de funções
+//function nomeFunção(parâmetros){corpo_da_função}
+
+// let num_A = 3.0;
+// let num_B = 2;
+// let num_C;
+
+// function imprimirResultado(operando1, operando2, operacao, resultado){
+//     console.log(`A ${operacao} entre ${operando1} e ${operando2} é igual a ${resultado}`);
+// }
+
+// //Arrays -> coleção de tipos primitivos, podem ser de tipos diferentes. 
+// //Declarando um array:
+// //let nome_array = []; Array vazio
+// const operacoes = ['adição', 'subtração', 'multiplicação', 'divisão'];
+// //Não é possível mudar uma constante do array, mas sim um índice, um elemento
+// num_C = num_A + num_B;
+// imprimirResultado(num_A, num_B, operacoes[0], num_C);
+// num_C = num_A - num_B;
+// imprimirResultado(num_A, num_B, operacoes[1], num_C);
+// num_C = num_A * num_B;
+// imprimirResultado(num_A, num_B, operacoes[2], num_C);
+// num_C = num_A / num_B;
+// imprimirResultado(num_A, num_B, operacoes[3], num_C);
+
+//Matriz -> Conjunto de arrays
+/*
+    1 2 3
+    4 5 6
+    7 8 9
+*/
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log(matriz);
+
+function imprimirMatriz(A){
+    for (let i = 0; i < A.length; i++){
+        
+    }
+}
--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -9,3 +9,20 @@ let mult = num_A * num_B;
 console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${mult}`);
 let div = num_A / num_B;
 console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${div}`);
+
+
+//Correção
+// let num_A = 2;
+// let num_B = 3.0;
+// let num_C;
+
+// num_C = num_A + num_B;
+// console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${num_C}`);
+// num_C = num_A - num_B;
+// console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${num_C}`);
+// num_C = num_A * num_B;
+// console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${num_C}`);
+// num_C = num_A / num_B;
+// console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${num_C}`);
+
+
