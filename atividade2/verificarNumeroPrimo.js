e553739792bc0bc0e87be725b05c23f53ccadd70
Mariacmv <mariaclaragmviana@gmail.com>
2025-03-18 09:54:17 -0300

    Correção Atividade 2.1

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -1,4 +1,4 @@
-function verificarNumeroPrimo(n) {
+function verificarNumeroPrimo(n) { //parâmetros são variáveis que são utilizadas apenas dentro da função
     if (n <= 1) {
         return false;
     }
@@ -10,13 +10,38 @@ function verificarNumeroPrimo(n) {
     return true;
 }
 
-console.log(verificarNumeroPrimo(0));
-console.log(verificarNumeroPrimo(1));
-console.log(verificarNumeroPrimo(2));
-console.log(verificarNumeroPrimo(3));
-console.log(verificarNumeroPrimo(7));
-console.log(verificarNumeroPrimo(83));
-console.log(verificarNumeroPrimo(100));
-console.log(verificarNumeroPrimo(991));
-console.log(verificarNumeroPrimo(104729));
-console.log(verificarNumeroPrimo(14348907));
+console.log(verificarNumeroPrimo(0));//false
+console.log(verificarNumeroPrimo(1));//false
+console.log(verificarNumeroPrimo(2));//true
+console.log(verificarNumeroPrimo(3));//true
+console.log(verificarNumeroPrimo(7));//true
+console.log(verificarNumeroPrimo(83));//true
+console.log(verificarNumeroPrimo(100));//false
+console.log(verificarNumeroPrimo(991));//true
+console.log(verificarNumeroPrimo(104729));//true
+console.log(verificarNumeroPrimo(14348907));//false
+
+//Importância dos números primos na computação:
+//  Exemplo: criptografia
+
+
+//Outra alternativa:
+function verificarNumeroPrimo(n){
+    if(n < 2){
+        return false;
+    }
+    if(n <= 3){
+        return true;
+    }
+    if(n % 2 == 0){
+        return false;
+    }
+
+    for(let i = 3; i < n; i += 2){
+        if(n % i == 0){
+            return false;
+        }
+    }
+
+    return true; //passado por todas as condições 
+}
