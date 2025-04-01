97cd38c442ed6405cffce9aa5ea3773fb3262de4
Maria Clara <135616880+Mariacmv@users.noreply.github.com>
2025-04-01 10:41:50 -0300

    Update calculadora.js

--- atividade3/util/calculadora.js
+++ atividade3/util/calculadora.js
@@ -1,16 +1,46 @@
-const somar = (a, b) => a + b;
-const subtrair = (a, b) => a - b;
-const multiplicar = (a, b) => a * b;
-const dividir = (a, b) => {
-    if (b == 0){
-        return 'ERRO: divisão por zero não é permitida';
-    }
-    return a/b;
-};
+// const somar = (a, b) => a + b;
+// const subtrair = (a, b) => a - b;
+// const multiplicar = (a, b) => a * b;
+// const dividir = (a, b) => {
+//     if (b == 0){
+//         return 'ERRO: divisão por zero não é permitida';
+//     }
+//     return a/b;
+// };
 
-module.exports = {
-    somar, 
-    subtrair, 
-    multiplicar, 
-    dividir
-};
+// module.exports = {
+//     somar, 
+//     subtrair, 
+//     multiplicar, 
+//     dividir
+// };
+
+//CORREÇÃO:
+
+/*
+    Esse módulo implementa as operções básicas de uma calculadora.
+    Essas funções devem ser exportadas
+*/
+
+export function somar(a, b){ //também pode passar no nome da função
+    return a + b;
+}
+
+export function subtrair(a, b){
+    return a - b;
+}
+
+export function multiplicar(a, b){
+    return a * b;
+}
+
+export function dividir(a, b){
+    return a / b;
+}
+
+// module.exports = {
+//     somar,
+//     subtrair, 
+//     multiplicar,
+//     dividir
+// }
